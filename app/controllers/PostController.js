c253258dc6bacc1989168dde7668a0018468a881
drogan120 <alimahmudin.hp@gmail.com>
2020-09-12 22:00:07 +0700

    adding update and delete on post

--- app/controllers/PostController.js
+++ app/controllers/PostController.js
@@ -43,13 +43,56 @@ exports.findAll = (req, res) => {
 };
 
 //  Find
-exports.find = (req, res) => {};
+exports.findOne = (req, res) => {
+  const id = req.params.id;
+  Post.findByPk(id)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        messages: err.messages || "failed to get data with id : " + id,
+      });
+    });
+};
 
 //Update
-exports.update = (req, res) => {};
+exports.update = (req, res) => {
+  const id = req.params.id;
+  Post.update(req.body, {
+    where: { id: id },
+  })
+    .then((result) => {
+      if (result == 1) {
+        res.send({ message: "post was updated successfully" });
+      } else {
+        res.send({ message: "cannot update post with id " + id });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        messages: err.messages || "failed to update data with id " + id,
+      });
+    });
+};
 
 // delete
-exports.destroy = (req, res) => {};
+exports.destroy = (req, res) => {
+  const id = req.params.id;
+  Post.destroy({ where: { id: id } })
+    .then((result) => {
+      if (result == 1) {
+        res.send({ message: "post has been deleted" });
+      } else {
+        res.send({ message: "failed to delete post" });
+      }
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .send({ message: err.message || "cannot delete post with id " + id });
+    });
+};
 
 // delete all
 exports.deleteAll = (req, res) => {};
--- app/routes/PostRoutes.js
+++ app/routes/PostRoutes.js
@@ -6,6 +6,9 @@ module.exports = (app) => {
   router.post("/", posts.create);
   // Retrieve all post
   router.get("/", posts.findAll);
+  router.get("/:id", posts.findOne);
+  router.put("/:id", posts.update);
+  router.delete("/:id", posts.destroy);
 
   app.use("/api/posts", router);
 };
